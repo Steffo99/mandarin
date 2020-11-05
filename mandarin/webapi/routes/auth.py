40e245f0a09cffbd7af1cfd3c99d4b829743d957
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-11-05 17:58:59 +0100

    ✏️ Rename routes in the auth router

--- mandarin/webapi/routes/auth.py
+++ mandarin/webapi/routes/auth.py
@@ -8,7 +8,7 @@ router_auth = f.APIRouter()
 
 
 @router_auth.get(
-    "/access_token",
+    "/token",
     summary="Validate the current access token.",
     responses={
         401: {"description": "Not logged in"},
@@ -19,7 +19,7 @@ def access_token(payload: dict = f.Depends(validate_access_token)):
 
 
 @router_auth.get(
-    "/current_user",
+    "/user",
     summary="Get info about the logged in user.",
     responses={
         401: {"description": "Not logged in"},
