b5e8631f2e5ab26f251531e19506b0581f0408e3
Stefano Pigozzi <me@steffo.eu>
2021-01-18 18:52:33 +0100

    📔 Add sphinx.ext.todo extension to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,7 @@ release = '0.2.0'
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
+    'sphinx.ext.todo',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
