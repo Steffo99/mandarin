7ea13f19e70d9fe079427cfd467a72681033e596
Stefano Pigozzi <me@steffo.eu>
2021-02-10 22:39:48 +0100

    🐛 Use create_all to reset the database

--- mandarin/webapi/routes/debug.py
+++ mandarin/webapi/routes/debug.py
@@ -1,12 +1,8 @@
 from __future__ import annotations
-from royalnet.typing import *
+
 import fastapi as f
 
-from ...database import tables, Base, lazy_engine
-from ...taskbus import tasks
-from .. import models
-from .. import dependencies
-from .. import responses
+from ...database import create_all, Base, lazy_engine
 
 router_debug = f.APIRouter()
 
@@ -20,10 +16,10 @@ def database_reset():
     """
     **Drop** and **recreate** all tables declared using the `DeclarativeBase` in `mandarin.database.base`.
 
-    **THIS DELETES ALL DATA FROM THE DATABASE!**
+    # THIS DELETES ALL DATA FROM THE DATABASE!
     """
     Base.metadata.drop_all(bind=lazy_engine.evaluate())
-    Base.metadata.create_all(bind=lazy_engine.evaluate())
+    create_all()
     return f.Response(status_code=204)
 
 
