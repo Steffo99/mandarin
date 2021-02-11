f146e90e26dad8cfd63101103e42afe698542723
Stefano Pigozzi <me@steffo.eu>
2021-02-11 04:21:30 +0100

    🧹 Remove server-side Genius stuff

--- mandarin/database/tables/people.py
+++ mandarin/database/tables/people.py
@@ -1,13 +1,9 @@
 from __future__ import annotations
 
-import lyricsgenius
-import lyricsgenius.types
 import royalnet.alchemist as a
 import sqlalchemy as s
 import sqlalchemy.orm as o
 
-from mandarin import exc
-from mandarin.genius import lazy_genius
 from ..base import Base
 
 
@@ -27,22 +23,6 @@ class Person(Base, a.ColRepr, a.Updatable, a.Makeable):
     __table_args__ = (
 
     )
-
-    def genius(self):
-        """
-        Update the fields of the song with information from Genius.
-
-        :raises FetchingError: If the song isn't found on Genius.
-        """
-        genius: lyricsgenius.Genius = lazy_genius.evaluate()
-
-        # Search for the song on Genius
-        data: lyricsgenius.types.Artist = genius.search_artist(artist_name=self.name, max_songs=0)
-        if data is None:
-            raise exc.GeniusError("No such song.")
-
-        self.name = data.name
-        self.description = data._body["description"]["plain"]
 
 
 __all__ = ("Person",)
--- mandarin/webapi/routes/debug.py
+++ mandarin/webapi/routes/debug.py
@@ -2,9 +2,8 @@ from __future__ import annotations
 
 import fastapi as f
 
-from . import songs as r_songs
 from .. import dependencies
-from ...database import create_all, Base, lazy_engine, tables
+from ...database import create_all, Base, lazy_engine
 
 router_debug = f.APIRouter()
 
@@ -24,41 +23,6 @@ def database_reset(
     """
     Base.metadata.drop_all(bind=lazy_engine.evaluate())
     create_all()
-    return f.Response(status_code=204)
-
-
-@router_debug.patch(
-    "/songs/genius",
-    summary="Fetch data from Genius for all songs in the database.",
-    status_code=204,
-    deprecated=True,
-)
-def songs_genius(
-        ls: dependencies.LoginSession = f.Depends(dependencies.dependency_login_session),
-        scrape_title: bool = f.Query(True, description="Whether song titles should be overwritten or not."),
-        scrape_description: bool = f.Query(True, description="Whether song descriptions should be overwritten or not."),
-        scrape_lyrics: bool = f.Query(False, description="Whether song lyrics should be overwritten or not."),
-        scrape_year: bool = f.Query(True, description="Whether song years should be overwritten or not."),
-):
-    """
-    **Replace** the content of all songs with data sourced from Genius with the `lyricsgenius` Python module.
-
-    # THIS IRREVERSIBLY OVERWRITES ALL SONG DATA!
-    """
-    for song in ls.session.query(tables.Song).all():
-        response = r_songs.get_genius(
-            ls=ls,
-            scrape_title=scrape_title,
-            scrape_description=scrape_description,
-            scrape_lyrics=scrape_lyrics,
-            scrape_year=scrape_year,
-            song_id=song.id,
-        )
-        r_songs.edit_single(
-            ls=ls,
-            song_id=song.id,
-            data=response
-        )
     return f.Response(status_code=204)
 
 
