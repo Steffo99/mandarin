7942bf430b9e43a7544151b20135f15c13245bb8
Stefano Pigozzi <me@steffo.eu>
2021-02-12 18:35:03 +0100

    🚧 Improve search API skeleton
    Time spent: 16 minutes

--- mandarin/webapi/models/b_extras.py
+++ mandarin/webapi/models/b_extras.py
@@ -2,6 +2,8 @@
 Extra models are :class:`.MandarinModel`\\ s which represent data not contained in the database.
 """
 
+import enum
+
 from . import a_base as base
 
 
@@ -15,6 +17,17 @@ class AuthConfig(base.MandarinModel):
     jwks: str
 
 
+class SearchableElementType(str, enum.Enum):
+    albums = "albums"
+    genres = "genres"
+    layers = "layers"
+    people = "people"
+    roles = "roles"
+    songs = "songs"
+    all = "all"
+
+
 __all__ = (
     "AuthConfig",
+    "SearchableElementType",
 )
